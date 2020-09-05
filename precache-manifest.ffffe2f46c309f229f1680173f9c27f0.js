ef3e7e5d619d91bd61d7cc300736fcde0d1016a7
sidra56 <sidraabdullah56@gmail.com>
2020-09-05 22:52:50 +0500

    Updates

---
+++ precache-manifest.ffffe2f46c309f229f1680173f9c27f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c07f6d97f1f1d583e1d1d504416638",
+    "url": "/Evento/index.html"
+  },
+  {
+    "revision": "3b8e83e2bb59a6082f6c",
+    "url": "/Evento/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0529ecb0547bca30c161",
+    "url": "/Evento/static/css/main.84f3186b.chunk.css"
+  },
+  {
+    "revision": "3b8e83e2bb59a6082f6c",
+    "url": "/Evento/static/js/2.483a38aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Evento/static/js/2.483a38aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0529ecb0547bca30c161",
+    "url": "/Evento/static/js/main.507add5e.chunk.js"
+  },
+  {
+    "revision": "fd2d439e7b43c225e8f0",
+    "url": "/Evento/static/js/runtime-main.ec7e350d.js"
+  },
+  {
+    "revision": "17b753c7d6c437a46a38677217535504",
+    "url": "/Evento/static/media/Event-img.17b753c7.png"
+  },
+  {
+    "revision": "c196ea3bf10e862f23043c62acc365ef",
+    "url": "/Evento/static/media/facebook.c196ea3b.svg"
+  },
+  {
+    "revision": "5fb11d870796168e8b9b3d33e0c0609d",
+    "url": "/Evento/static/media/sponsor3.5fb11d87.jpg"
+  },
+  {
+    "revision": "6ba7214f9549a57857566bc6d1f572ea",
+    "url": "/Evento/static/media/twitter.6ba7214f.svg"
+  },
+  {
+    "revision": "f278b3d132ec7724304ef1842071c606",
+    "url": "/Evento/static/media/whatsapp.f278b3d1.svg"
+  }
+]);
